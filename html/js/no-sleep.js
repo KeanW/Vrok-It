c57f94a98e9f287a47a65e7a4a09ceb3b622d5cc
Kean Walmsley <kean.walmsley@autodesk.com>
2015-11-24 19:58:40 +0100

    Stubbing toes

--- html/js/no-sleep.js
+++ html/js/no-sleep.js
@@ -2,18 +2,18 @@
 (function(global) {
 
 	function NoSleep() {
-		this._video = document.createElement('video');
+		//this._video = document.createElement('video');
 		//window.addEventListener('touchstart', this.enable.bind(this));
 	};
 
 	NoSleep.prototype.enable = function() {
-		this._video.src = 'no-sleep.webm';
-		this._video.play();
+		//this._video.src = 'no-sleep.webm';
+		//this._video.play();
 	};
 
 	NoSleep.prototype.disable = function() {
-		this._video.pause();
-		this._video.src = '';
+		//this._video.pause();
+		//this._video.src = '';
 	};
 
 	global.NoSleep = NoSleep;
