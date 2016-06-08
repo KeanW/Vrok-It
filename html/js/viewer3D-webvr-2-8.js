ef6651d179cc9b507a62a101f4b61236b39155d8
Kean Walmsley <kean.walmsley@autodesk.com>
2016-06-08 09:35:51 +0200

    Should fix it

--- html/js/viewer3D-webvr-2-8.js
+++ html/js/viewer3D-webvr-2-8.js
@@ -51190,7 +51190,7 @@ function Viewer3DImpl(thecanvas, theapi)
     //Main animation loop -- update camera,
     //advance animations, render if needed.
     // Next line modified by KRW to support WebVR
-    self.tick = function(highResTimeStamp) {
+    this.tick = function(highResTimeStamp) {
 
         //Did the window resize since last tick?
         if (_needsResize) updateCanvasSize();
